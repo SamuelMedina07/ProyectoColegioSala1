e5b9a0867ff6db3856ebae56f9e66608c877564c
Samuel <samuelmedina2000@gmail.com>
2026-07-28 21:07:42 -0600

    Conexion 192.168.40.156 correcta

--- src/modelo/Conexion.java
+++ src/modelo/Conexion.java
@@ -18,7 +18,7 @@ private String db = "bd_colegio_sala1";
 private String user = "crud_java";
 private String pass = "CrudJava2026";
 private String ip = "192.168.40.156";
-private String url = "jdbc:mysql://" + ip + " /" + db;
+private String url = "jdbc:mysql://" + ip + "/" + db;
 
     public Connection getConnection(){
         try {
